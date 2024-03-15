2e36d425869d28b4023a864ee7e94ff6810ecb0b
Jaime Bernal <jaimejair.bernal@corporativopmn.com>
2024-03-14 18:14:24 -0600

    Diseño

---
+++ src/components/TituloH1.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 :class="clases">{{ titulo }}</h1>
+</template>
+
+<script>
+export default {
+  props: {
+    titulo: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    clases() {
+      return "text-3xl font-bold text-gray-800 text-center";
+    },
+  },
+};
+</script>
