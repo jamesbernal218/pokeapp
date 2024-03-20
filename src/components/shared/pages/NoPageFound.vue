5f8946fb29a93692161dbe622086036faf8d6ec8
Jaime Bernal <jaimejair.bernal@corporativopmn.com>
2024-03-19 21:40:30 -0600

    Pagina de ruta no encontrada ok

---
+++ src/components/shared/pages/NoPageFound.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="min-h-screen flex flex-grow items-center justify-center bg-gradient-to-br from-blue-300 via-blue-500 to-blue-700"
+  >
+    <div class="rounded-lg bg-white p-8 text-center shadow-xl">
+      <h1 class="mb-4 text-4xl font-bold text-gray-800">404</h1>
+      <p class="text-gray-700 mb-4">
+        Oops! Parece que has entrado a una zona inexplorada.
+      </p>
+      <SnorlaxImg />
+      <BotonReturnBack />
+    </div>
+  </div>
+</template>
+<script>
+import SnorlaxImg from "@/components/IMG/SnorlaxImg.vue";
+import BotonReturnBack from "@/components/botones/BotonReturnBack.vue";
+export default {
+  components: {
+    SnorlaxImg,
+    BotonReturnBack,
+  },
+};
+</script>
--- src/router/router.js
+++ src/router/router.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import NavdarApp from '../components/pokemon/pages/NavdarApp.vue'
+import NoPageFound from '@/components/shared/pages/NoPageFound.vue'
 
 const routes = [
     { path: '/', component: NavdarApp },
+    { path: '/:pathMatch(.*)*', component: NoPageFound },
 ]
 
 const router = createRouter({
