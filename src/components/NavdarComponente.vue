28cbff5d71407ce012fae87cdd602c9c565493d6
Jaime Bernal <jaimejair.bernal@corporativopmn.com>
2024-03-19 18:09:12 -0600

    componente en vista de Navdar

--- src/components/NavdarComponente.vue
+++ src/components/NavdarComponente.vue
@@ -1,11 +1,7 @@
 <template>
   <nav class="bg-white border-gray-200 dark:bg-gray-900">
     <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
-      <img
-        src="https://i.pinimg.com/564x/33/22/90/332290edcf0acb810b012dd8b9caa491.jpg"
-        class="h-8"
-        alt="Flowbite Logo"
-      />
+      <ImgPokemon />
       <div class="flex items-center space-x-2">
         <WhatsappIcon />
         <a
@@ -22,21 +18,7 @@
       <div class="flex items-center">
         <ul class="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
           <li>
-            <a
-              href="#"
-              class="text-gray-900 dark:text-white hover:underline"
-              aria-current="page"
-              >Home</a
-            >
-          </li>
-          <li>
-            <a href="#" class="text-gray-900 dark:text-white hover:underline">Company</a>
-          </li>
-          <li>
             <a href="#" class="text-gray-900 dark:text-white hover:underline">Team</a>
-          </li>
-          <li>
-            <a href="#" class="text-gray-900 dark:text-white hover:underline">Features</a>
           </li>
         </ul>
       </div>
@@ -46,9 +28,11 @@
 
 <script>
 import WhatsappIcon from "./Svg/WhatsappIcon.vue";
+import ImgPokemon from "./IMG/ImgPokemon.vue";
 export default {
   components: {
     WhatsappIcon,
+    ImgPokemon,
   },
 };
 </script>
