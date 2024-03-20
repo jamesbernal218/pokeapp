bfc81fa7b264215e869f38046a249c9eb1ce0edc
Jaime Bernal <jaimejair.bernal@corporativopmn.com>
2024-03-19 21:48:25 -0600

    Formato ok

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import NavdarApp from '../components/pokemon/pages/NavdarApp.vue'
-import NoPageFound from '@/components/shared/pages/NoPageFound.vue'
+import NoPageFound from '../components/shared/pages/NoPageFound.vue'
 
 const routes = [
     {
